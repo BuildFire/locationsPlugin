ba4f795b830ca445d5997dde928d73a7fec5d27f
mhd-rawashdah <mhd.rawashdah@gmail.com>
2021-11-25 16:31:47 +0200

    fix regex issue (charset)

--- src/control/content/js/locations/index.js
+++ src/control/content/js/locations/index.js
@@ -41,6 +41,34 @@ const renderAddLocationsPage = () => {
 
 window.addEditLocation = (location, callback = () => {}) => {
   renderAddLocationsPage();
+  loadMap();
+};
+
+window.intiMap = () => {
+  console.log('Map Ready');
+  let googleMap = new google.maps.Map(document.getElementById('location-map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 10,
+    zoomControl: true,
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false,
+    gestureHandling: 'greedy',
+  });
+};
+
+const loadMap = () => {
+  buildfire.getContext((error, context) => {
+    function setGoogleMapsScript(key) {
+      console.log(key);
+      const docHead = document.getElementsByTagName("head");
+      const scriptEl = document.createElement("script");
+      scriptEl.id = "googleScript";
+      scriptEl.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=intiMap`;
+      docHead[0].appendChild(scriptEl);
+    }
+    setGoogleMapsScript(context.apiKeys.googleMapKey);
+  });
 };
 
 const loadLocations = () => {
