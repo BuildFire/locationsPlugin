09a5c382e85e77b87757ded0e786e38c42df99db
Mahmoud AlSharif <mahmoud.k.alsharif@gmail.com>
2022-03-09 19:52:45 +0300

    feat(widget): Languages Support

---
+++ src/shared/stringsConfig.js
@@ -0,0 +1,232 @@
+export default {
+  general: {
+    title: 'General',
+    labels: {
+      search: {
+        title: 'Search',
+        placeholder: 'Enter text here',
+        defaultValue: 'Search',
+        isPlaceHolder: true,
+        maxLength: 20,
+      },
+      price: {
+        title: 'Price',
+        placeholder: 'Enter text here',
+        defaultValue: 'Price',
+        maxLength: 10,
+      },
+      reset: {
+        title: 'Reset',
+        placeholder: 'Enter text here',
+        defaultValue: 'Reset',
+        maxLength: 10,
+      },
+      open: {
+        title: 'Open',
+        placeholder: 'Enter text here',
+        defaultValue: 'Open',
+        maxLength: 8,
+      },
+      closed: {
+        title: 'Closed',
+        placeholder: 'Enter text here',
+        defaultValue: 'Closed',
+        maxLength: 8,
+      },
+      bookmarks: {
+        title: 'Bookmarks',
+        placeholder: 'Enter text here',
+        defaultValue: 'Bookmarks',
+        maxLength: 12,
+      },
+      bookmarkResults: {
+        title: 'Bookmark Results',
+        placeholder: 'Enter text here',
+        defaultValue: 'Bookmark Results',
+        maxLength: 12,
+      },
+      rateThisLocation: {
+        title: 'Rate This Location',
+        placeholder: 'Enter text here',
+        defaultValue: 'Rate This Location',
+        maxLength: 40,
+      },
+      findWithinThisArea: {
+        title: 'Find Within This Area',
+        placeholder: 'Enter text here',
+        defaultValue: 'Find within this area',
+        maxLength: 25,
+      },
+      distanceUnitKm: {
+        title: 'Kilometre Distance Unit',
+        placeholder: 'Enter text here',
+        defaultValue: 'KM',
+        maxLength: 2,
+      },
+      distanceUnitMi: {
+        title: 'Mile Distance Unit',
+        placeholder: 'Enter text here',
+        defaultValue: 'mi',
+        maxLength: 2,
+      },
+      distanceUnitFt: {
+        title: 'Feet Distance Unit',
+        placeholder: 'Enter text here',
+        defaultValue: 'ft',
+        maxLength: 2,
+      }
+    }
+  },
+  sortingOptions: {
+    title: 'Sorting Options',
+    labels: {
+      alphabeticalAsc: {
+        title: 'Alphabetical Ascending',
+        placeholder: 'Enter text here',
+        defaultValue: 'Title (A-Z)',
+        maxLength: 12,
+      },
+      alphabeticalDsc: {
+        title: 'Alphabetical Descending',
+        placeholder: 'Enter text here',
+        defaultValue: 'Title (Z-A)',
+        maxLength: 12,
+      },
+      distance: {
+        title: 'Distance',
+        placeholder: 'Enter text here',
+        defaultValue: 'Distance',
+        maxLength: 12,
+      },
+      recommended: {
+        title: 'Recommended',
+        placeholder: 'Enter text here',
+        defaultValue: 'Recommended',
+        maxLength: 12,
+      },
+      priceDsc: {
+        title: 'Price Descending',
+        placeholder: 'Enter text here',
+        defaultValue: '$$$ - $',
+        maxLength: 12,
+      },
+      priceAsc: {
+        title: 'Price Ascending',
+        placeholder: 'Enter text here',
+        defaultValue: '$ - $$$',
+        maxLength: 12,
+      },
+      recent: {
+        title: 'Recent',
+        placeholder: 'Enter text here',
+        defaultValue: 'Recent',
+        maxLength: 12,
+      },
+      mostViewed: {
+        title: 'Most Viewed',
+        placeholder: 'Enter text here',
+        defaultValue: 'Most Viewed',
+        maxLength: 12,
+      }
+    }
+  },
+  openingHoursDialog: {
+    title: 'Opening Hours Dialog',
+    labels: {
+      openHours: {
+        title: 'Open Hours',
+        placeholder: 'Enter text here',
+        defaultValue: 'Open Hours',
+        maxLength: 25,
+      },
+      monday: {
+        title: 'Monday',
+        placeholder: 'Enter text here',
+        defaultValue: 'Monday',
+        maxLength: 25,
+      },
+      sunday: {
+        title: 'Sunday',
+        placeholder: 'Enter text here',
+        defaultValue: 'Sunday',
+        maxLength: 25,
+      },
+      tuesday: {
+        title: 'Tuesday',
+        placeholder: 'Enter text here',
+        defaultValue: 'Tuesday',
+        maxLength: 25,
+      },
+      wednesday: {
+        title: 'Wednesday',
+        placeholder: 'Enter text here',
+        defaultValue: 'Wednesday',
+        maxLength: 25,
+      },
+      thursday: {
+        title: 'Thursday',
+        placeholder: 'Enter text here',
+        defaultValue: 'Thursday',
+        maxLength: 25,
+      },
+      friday: {
+        title: 'Friday',
+        placeholder: 'Enter text here',
+        defaultValue: 'Friday',
+        maxLength: 25,
+      },
+      saturday: {
+        title: 'Saturday',
+        placeholder: 'Enter text here',
+        defaultValue: 'Saturday',
+        maxLength: 25,
+      },
+    }
+  },
+  toast: {
+    title: 'Toast Messages',
+    labels: {
+      bookmarksRemoved: {
+        title: 'Bookmark Removed',
+        placeholder: 'Enter text here',
+        defaultValue: 'Bookmark removed',
+        maxLength: 25,
+      },
+      bookmarksAdded: {
+        title: 'Bookmark Added',
+        placeholder: 'Enter text here',
+        defaultValue: 'Bookmark added',
+        maxLength: 25,
+      },
+      bookmarksError: {
+        title: 'Couldn\'t bookmark',
+        placeholder: 'Enter text here',
+        defaultValue: 'Couldn\'t bookmark',
+        maxLength: 25,
+      }
+    }
+  },
+  emptyState: {
+    title: 'Empty State',
+    labels: {
+      emptyCategories: {
+        title: 'Quick Filter',
+        placeholder: 'Enter text here',
+        defaultValue: 'No Categories Added',
+        maxLength: 25,
+      },
+      locationsListBeforeSearch: {
+        title: 'Locations List (Pre-Search)',
+        placeholder: 'Enter text here',
+        defaultValue: 'Start your search',
+        maxLength: 40,
+      },
+      locationsListAfterSearch: {
+        title: 'Locations List (Post-Search)',
+        placeholder: 'Enter text here',
+        defaultValue: 'No locations found within this area',
+        maxLength: 40,
+      }
+    }
+  },
+};
