f16c470ff34405b1d4831d9a19cd8d6244342142
pierrekamel5 <pierrekamel57@yahoo.com>
2022-09-28 21:38:58 +0300

    bug fixes

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -265,7 +265,7 @@ const searchLocations = () => {
         result.sort((a, b) => a.distance.split(" ")[0] - b.distance.split(" ")[0]);
         state.listLocations.sort((a, b) => a.distance.split(" ")[0] - b.distance.split(" ")[0]);
       }
-   
+
       if (state.searchCriteria.searchValue && !state.listLocations.length && !state.nearestLocation && nearestLocation && state.checkNearLocation) {
         state.nearestLocation = nearestLocation;
         state.checkNearLocation  = false;
@@ -285,7 +285,7 @@ const searchLocations = () => {
       mapView.renderListingLocations(result);
       result.forEach((location) => state.maps.map.addMarker(location, handleMarkerClick));
 
-      if (!state.fetchingEndReached && state.listLocations.length <= 200) {
+      if (!state.fetchingEndReached) {
         state.searchCriteria.page += 1;
         return searchLocations();
       }
@@ -441,7 +441,7 @@ const clearAndSearchAllLocation = () => {
     WidgetController.searchLocations(options).then((response)=>{
       state.listLocations = response.result;
       renderIntroductoryLocations();
-      if(state.listLocations.length == 0){
+      if(state.listLocations.length == 0 && (!state.pinnedLocations.length || state.pinnedLocations.length == 0)){
         showElement("div.empty-page");
       }
       mapView.clearMapViewList();
@@ -459,7 +459,7 @@ const renderIntroductoryLocations = () => {
     introView.clearIntroViewList();
     fetchPinnedLocations(() => {
       introView.renderIntroductoryLocations(state.listLocations, true);
-      if(state.listLocations.length == 0){
+      if(state.listLocations.length == 0 && (!state.pinnedLocations.length || state.pinnedLocations.length == 0)){
         showElement("div.empty-page");
       }
     });
@@ -638,7 +638,7 @@ const showLocationDetail = () => {
           </span>
         </div>
       </div>`).join('\n');
-      selectors.carousel.innerHTML = selectedLocation.images.map((n) => `<div style="background-image: url(${buildfire.imageLib.cropImage(n.imageUrl,{ size: "full_width", aspect: "1:1"})});" data-id="${n.id}"></div>`).join('\n');
+      selectors.carousel.innerHTML = selectedLocation.images.map((n) => `<div style="background-image: url('${buildfire.imageLib.cropImage(n.imageUrl,{ size: "full_width", aspect: "1:1"})}');" data-id="${n.id}"></div>`).join('\n');
       addBreadcrumb({ pageName: 'detail', title: 'Location Detail' });
       resetBodyScroll();
       navigateTo('detail');
@@ -707,9 +707,15 @@ const handleDetailActionItem = (e) => {
 };
 const handleListActionItem = (e) => {
   const actionItemId = e.target.dataset.actionId;
-  const actionItem = state.listLocations
+  var actionItem = state.listLocations
     .reduce((prev, next) => prev.concat(next.actionItems), [])
     .find((entity) => entity.id === actionItemId);
+
+  if(!actionItem){
+     actionItem = state.pinnedLocations
+    .reduce((prev, next) => prev.concat(next.actionItems), [])
+    .find((entity) => entity.id === actionItemId);
+  }
   buildfire.actionItems.execute(
     actionItem,
     (err) => {
@@ -1763,7 +1769,8 @@ const handleCPSync = (message) => {
           refreshIntroductoryCarousel();
           if (state.settings.introductoryListView.images.length === 0
             && state.listLocations.length === 0
-            && !state.settings.introductoryListView.description) {
+            && !state.settings.introductoryListView.description
+            && (!state.pinnedLocations.length || state.pinnedLocations.length == 0)) {
             showElement('#intro div.empty-page');
           }
           // eslint-disable-next-line no-new
